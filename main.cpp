04f460b714468db811e98beee883ec0a554e56f8
Ashley Diaz <nlechakra@outlook.com>
2025-09-05 21:13:17 +0000

    pass

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main() {
   double perimeter = 2 * length + 2 * width;
 
 cout << "Rectangle Properties:" <<endl;
-cout << "Length = 7.2" <<endl;
-cout << "Width = 1.3" <<endl;
+cout << " Length = 7.2" <<endl;
+cout << " Width = 1.3" <<endl;
 cout << "Area = " << area <<endl;
 cout << "Perimeter = " << perimeter <<endl;
 }
