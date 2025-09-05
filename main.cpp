f5a7bb06d8a4ea94d0ed2bb59184d507fac61d2d
Ashley Diaz <nlechakra@outlook.com>
2025-09-05 21:08:41 +0000

    done

--- main.cpp
+++ main.cpp
@@ -5,5 +5,12 @@ int main() {
   const double length = 7.2;
   const double width = 1.3;
 
+  double area = width * length;
+  double perimeter = 2 * length + 2 * width;
 
+cout << "Rectangle Properties:" <<endl;
+cout << "Length = 7.2" <<endl;
+cout << "Width = 1.3" <<endl;
+cout << "Area = " << area <<endl;
+cout << "Perimeter = " << perimeter <<endl;
 }
